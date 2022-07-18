0662842fb63ff63dfc4159853e0048b66dd17287
Mayank Jatav <MayankJatav7898@Gmail.Com>
2022-07-18 22:47:16 +0530

    Added Run Button In The Action Bar

--- app/src/main/java/com/example/androide/MainActivity.java
+++ app/src/main/java/com/example/androide/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.androide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.run_code:
+                Toast.makeText(this, "Run Code", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
